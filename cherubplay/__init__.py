8df9825d08f9a7d51b1da9f924f57447372df0b8
Olly Parton <mysticdragonfly@hotmail.co.uk>
2013-11-17 14:45:28 +0000

    Routes.

--- cherubplay/__init__.py
+++ cherubplay/__init__.py
@@ -8,14 +8,29 @@ from .models import (
 
 
 def main(global_config, **settings):
-    """ This function returns a Pyramid WSGI application.
-    """
+
     engine = engine_from_config(settings, 'sqlalchemy.')
     Session.configure(bind=engine)
     Base.metadata.bind = engine
+
     config = Configurator(settings=settings)
-    config.include('pyramid_chameleon')
+
     config.add_static_view('static', 'static', cache_max_age=3600)
+
     config.add_route('home', '/')
+
+    config.add_route('sign_up', '/sign-up/')
+    config.add_route('log_in', '/log-in/')
+    config.add_route('log_out', '/log-out/')
+
+    config.add_route('chat_list', '/chats/')
+    config.add_route('chat_list_archive', '/chats/archive/')
+
+    config.add_route('chat', '/chats/{url}/')
+    config.add_route('chat_archive', '/chats/url}/archive/')
+
+    config.add_route('account', '/account/')
+
     config.scan()
     return config.make_wsgi_app()
+
