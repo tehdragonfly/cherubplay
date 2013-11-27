a8f8709e9213c1f43b0f55c4e042e54e530a70eb
Olly Parton <mysticdragonfly@hotmail.co.uk>
2013-11-27 21:06:23 +0000

    Typing/stopped typing messages.

--- cherubplay/static/chat.js
+++ cherubplay/static/chat.js
@@ -1,8 +1,11 @@
 
 var colour_regex = /^#[0-9a-f]{6}$/i;
+var typing = false;
+var typing_timeout;
 
 var messages = $("#messages");
 var status_bar = $("#status_bar");
+var last_message_time = status_bar.text();
 var message_form_container = $("#message_form_container");
 var message_form = $("#message_form");
 var message_colour = $("#message_colour").change(function() {
@@ -12,6 +15,16 @@ var message_text = $("#message_text").keypress(function(e) {
 	if (e.keyCode==13 && !e.shiftKey) {
 		message_form.submit();
 		return false;
+	} else {
+		window.clearTimeout(typing_timeout);
+		if (!typing) {
+			typing = true;
+			ws.send("{\"action\":\"typing\"}");
+		}
+		typing_timeout = window.setTimeout(function() {
+			typing = false;
+			ws.send("{\"action\":\"stopped_typing\"}");
+		}, 1000);
 	}
 });
 var end_form = $("#end_form");
@@ -45,6 +58,8 @@ var ws = new WebSocket("ws://www.cherubplay.tk/live/"+chat_url+"/");
 ws.onopen = function(e) {
 	window.setTimeout(ping, 8000);
 	message_form.submit(function() {
+		typing = false;
+		window.clearTimeout(typing_timeout);
 		if (!colour_regex.test(message_colour.val())) {
 			alert("The colour needs to be a valid hex code, for example \"#0715CD\" or \"#416600\".");
 			return false;
@@ -84,11 +99,18 @@ ws.onmessage = function(e) {
 	message = JSON.parse(e.data);
 	if (message.action=="message") {
 		render_message(message.message);
+		var d = new Date();
+		last_message_time = "Last message: "+d.toLocaleDateString()+" "+d.toLocaleTimeString()
+		status_bar.text(last_message_time);
 	} else if (message.action=="end") {
 		ws.close();
 		status_bar.remove();
 		message_form_container.remove();
 		render_message(message.message);
+	} else if (message.action=="typing") {
+		status_bar.text(message.symbol+" is typing.");
+	} else if (message.action=="stopped_typing") {
+		status_bar.text(last_message_time);
 	}
 }
 
