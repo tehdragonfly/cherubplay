e0840c4c8e989dd279692b762e9743a4518c4f3d
Olly Parton <mysticdragonfly@hotmail.co.uk>
2013-11-27 01:08:04 +0000

    Prompting and searching.

--- cherubplay_live/search.py
+++ cherubplay_live/search.py
@@ -1,3 +1,7 @@
+import json
+
+from uuid import uuid4
+
 from sqlalchemy.orm.exc import NoResultFound
 from tornado.gen import engine, Task
 from tornado.ioloop import IOLoop
@@ -6,16 +10,80 @@ from tornado.websocket import WebSocketHandler
 
 from db import get_user
 
+prompters = {}
+searchers = {}
+
+def write_message_to_searchers(message):
+    for socket in searchers.values():
+        socket.write_message(message)
+
 class SearchHandler(WebSocketHandler):
+
     def open(self):
         self.user = get_user(self.cookies)
         if self.user is None:
             self.close()
             return
+        self.socket_id = str(uuid4())
+        print "NEW SOCKET: ", self.socket_id
         self.state = "idle"
+        self.write_message(json.dumps({ "username": self.user.username}))
+
+    def reset_state(self):
+        if self.socket_id in prompters:
+            prompters.pop(self.socket_id)
+            write_message_to_searchers(json.dumps({
+                "action": "remove_prompt",
+                "id": self.socket_id,
+            }))
+            print "PROMPTERS:", prompters
+        if self.socket_id in searchers:
+            searchers.pop(self.socket_id)
+            print "SEARCHERS:", searchers
+
+    def on_message(self, message_string):
+        try:
+            message = json.loads(message_string)
+        except ValueError:
+            return
+        if message["action"]=="search":
+            self.reset_state()
+            self.state = "searching"
+            searchers[self.socket_id] = self
+            print "SEARCHERS:", searchers
+            self.write_message(json.dumps({
+                "action": "prompts",
+                "prompts": [
+                    {
+                        "id": _.socket_id,
+                        "colour": _.colour,
+                        "prompt": _.prompt,
+                    } for _ in prompters.values()
+                ]
+            }))
+        elif message["action"]=="prompt":
+            self.reset_state()
+            self.state = "prompting"
+            prompters[self.socket_id] = self
+            print "PROMPTERS:", prompters
+            # XXX HEX CODE VALIDATION
+            self.colour = message["colour"]
+            self.prompt = message["prompt"]
+            write_message_to_searchers(json.dumps({
+                "action": "new_prompt",
+                "id": self.socket_id,
+                "colour": self.colour,
+                "prompt": self.prompt,
+            }))
+        elif message["action"]=="idle":
+            self.reset_state()
+            self.state = "idle"
+
+    def on_close(self):
+        self.reset_state()
 
 def main():
-    application = Application([(r"/search/", SearchHandler)])
+    application = Application([(r"/", SearchHandler)])
     application.listen(8000)
     IOLoop.instance().start()
 
