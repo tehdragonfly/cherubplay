454a222a0bd12f81d342bfdb6433d949cd27c310
Olly Parton <mysticdragonfly@hotmail.co.uk>
2014-04-05 18:06:08 +0100

    Prompt categories.

--- cherubplay_live/search.py
+++ cherubplay_live/search.py
@@ -10,18 +10,18 @@ from tornado.netutil import bind_unix_socket
 from tornado.web import Application, HTTPError
 from tornado.websocket import WebSocketHandler
 
-from cherubplay.lib import colour_validator
+from cherubplay.lib import colour_validator, prompt_categories
 from cherubplay.models import Chat, ChatUser, Message
 
 from db import config, get_user, sm
 
 prompters = {}
 searchers = {}
-nsfw_searchers = {}
 
-def write_message_to_searchers(message, nsfw):
-    for socket in (nsfw_searchers if nsfw else searchers).values():
-        socket.write_message(message)
+def write_message_to_searchers(message, category):
+    for socket in searchers.values():
+        if socket.category==category:
+            socket.write_message(message)
 
 class SearchHandler(WebSocketHandler):
 
@@ -41,14 +41,11 @@ class SearchHandler(WebSocketHandler):
             write_message_to_searchers(json.dumps({
                 "action": "remove_prompt",
                 "id": self.socket_id,
-            }), self.nsfw)
+            }), self.category)
             print "PROMPTERS:", prompters
         if self.socket_id in searchers:
             searchers.pop(self.socket_id)
             print "SEARCHERS:", searchers
-        if self.socket_id in nsfw_searchers:
-            nsfw_searchers.pop(self.socket_id)
-            print "NSFW SEARCHERS:", nsfw_searchers
 
     def on_message(self, message_string):
         try:
@@ -58,11 +55,9 @@ class SearchHandler(WebSocketHandler):
         if message["action"]=="search":
             self.reset_state()
             self.state = "searching"
+            self.category = message["category"]
             searchers[self.socket_id] = self
             print "SEARCHERS:", searchers
-            if message["nsfw"]:
-                nsfw_searchers[self.socket_id] = self
-                print "NSFW SEARCHERS:", nsfw_searchers
             self.write_message(json.dumps({
                 "action": "prompts",
                 "prompts": [
@@ -70,10 +65,10 @@ class SearchHandler(WebSocketHandler):
                         "id": _.socket_id,
                         "colour": _.colour,
                         "prompt": _.prompt,
-                        "nsfw": _.nsfw,
+                        "category": _.category,
                     }
                     for _ in prompters.values()
-                    if message["nsfw"] or not _.nsfw
+                    if _.category==message["category"]
                 ]
             }))
         elif message["action"]=="prompt":
@@ -90,6 +85,12 @@ class SearchHandler(WebSocketHandler):
                     "error": "You can't submit a blank prompt.",
                 }))
                 return
+            if message["category"] not in prompt_categories:
+                self.write_message(json.dumps({
+                    "action": "prompt_error",
+                    "error": "The specified category doesn't seem to exist.",
+                }))
+                return
             if "Oan you believe this is happening?" in message["prompt"]:
                 for line in message["prompt"].split("\n"):
                     if line.strip()!="":
@@ -104,14 +105,14 @@ class SearchHandler(WebSocketHandler):
             print "PROMPTERS:", prompters
             self.colour = message["colour"]
             self.prompt = message["prompt"]
-            self.nsfw = message["nsfw"]
+            self.category = message["category"]
             write_message_to_searchers(json.dumps({
                 "action": "new_prompt",
                 "id": self.socket_id,
                 "colour": self.colour,
                 "prompt": self.prompt,
-                "nsfw": self.nsfw,
-            }), self.nsfw)
+                "category": self.category,
+            }), self.category)
         elif message["action"]=="idle":
             self.reset_state()
             self.state = "idle"
