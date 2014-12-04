31f22497b8beaa125e8690bee79088d1ad0eb82a
Olly Parton <mysticdragonfly@hotmail.co.uk>
2014-12-04 15:43:24 +0000

    Automatically add trailing slashes to URLs.

---
+++ cherubplay/templates/errors/not_found.mako
@@ -0,0 +1,3 @@
+<%inherit file="../base.mako" />\
+  <h2>404 Not found</h2>
+  <p>The resource <strong>${request.path}</strong> could not be found.</p>
--- cherubplay/views/errors.py
+++ cherubplay/views/errors.py
@@ -1,6 +1,6 @@
 from pyramid.httpexceptions import HTTPForbidden
 from pyramid.renderers import render_to_response
-from pyramid.view import forbidden_view_config
+from pyramid.view import forbidden_view_config, notfound_view_config
 
 
 @forbidden_view_config()
@@ -9,4 +9,9 @@ def forbidden(request):
         return render_to_response("errors/forbidden.mako", {}, request=request)
     else:
         return render_to_response("home_guest.mako", { "forbidden": True }, request=request)
+
+
+@notfound_view_config(append_slash=True)
+def not_found(request):
+    return render_to_response("errors/not_found.mako", {}, request=request)
 
