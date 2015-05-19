7bfcd3364dcbbf1abb73cd190bdb63bdde2dc201
Olly Parton <mysticdragonfly@hotmail.co.uk>
2015-05-19 22:51:03 +0100

    Set status codes explicitly on error views.

--- cherubplay/views/errors.py
+++ cherubplay/views/errors.py
@@ -6,12 +6,16 @@ from pyramid.view import forbidden_view_config, notfound_view_config
 @forbidden_view_config()
 def forbidden(request):
     if request.user is not None:
-        return render_to_response("errors/forbidden.mako", {}, request=request)
+        resp = render_to_response("errors/forbidden.mako", {}, request=request)
+        resp.status = "403 Forbidden"
+        return resp
     else:
         return render_to_response("home_guest.mako", { "forbidden": True }, request=request)
 
 
 @notfound_view_config(append_slash=True)
 def not_found(request):
-    return render_to_response("errors/not_found.mako", {}, request=request)
+    resp = render_to_response("errors/not_found.mako", {}, request=request)
+    resp.status = "404 Not Found"
+    return resp
 
