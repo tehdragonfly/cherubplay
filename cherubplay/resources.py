a066374094d72bf2cfb45ea7417fdc173f6c83a7
Olly Parton <mysticdragonfly@hotmail.co.uk>
2015-11-27 21:31:07 +0000

    Use joinedload on request pages.

--- cherubplay/resources.py
+++ cherubplay/resources.py
@@ -1,8 +1,9 @@
 from pyramid.httpexceptions import HTTPNotFound
 from sqlalchemy import and_, or_
+from sqlalchemy.orm import joinedload_all
 from sqlalchemy.orm.exc import NoResultFound
 
-from .models import Session, Prompt, Request
+from .models import Session, Prompt, Request, RequestTag
 
 
 def prompt_factory(request):
@@ -17,14 +18,15 @@ def prompt_factory(request):
 
 def request_factory(request):
     try:
-        # XXX joinedload
         return Session.query(Request).filter(and_(
             Request.id == int(request.matchdict["id"]),
             or_(
                 Request.status == "posted",
                 Request.user_id == request.user.id,
             ),
-        )).one()
+        )).options(
+            joinedload_all(Request.tags, RequestTag.tag)
+        ).one()
     except (ValueError, NoResultFound):
         raise HTTPNotFound
 
