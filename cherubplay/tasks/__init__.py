2e55ea3c554b0039e66b647d7e583c7a8abc8266
Olly Parton <mysticdragonfly@hotmail.co.uk>
2016-11-11 23:40:07 +0000

    Don't use ScopedSession in tasks.

--- cherubplay/tasks/__init__.py
+++ cherubplay/tasks/__init__.py
@@ -1,39 +1,51 @@
-import transaction
-
+from contextlib import contextmanager
 from datetime import datetime, timedelta
 from pyramid_celery import celery_app as app
 from sqlalchemy import and_, engine_from_config, func
+from sqlalchemy.dialects.postgresql import INTERVAL
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql.expression import cast
 
-from cherubplay.models import Session, Request, RequestTag, User
+from cherubplay.models import Request, RequestTag, User
+
+
+engine = engine_from_config(app.conf["PYRAMID_REGISTRY"].settings, "sqlalchemy.")
+sm = sessionmaker(bind=engine, autoflush=False)
+
+
+@contextmanager
+def db_session():
+    db = sm()
+    try:
+        yield db
+        db.commit()
+    except:
+        db.rollback()
+        raise
+    finally:
+        db.close()
+
 
 @app.task
 def reap_requests():
-    engine = engine_from_config(app.conf["PYRAMID_REGISTRY"].settings, "sqlalchemy.")
-    # TODO don't use scoped_session
-    Session.configure(bind=engine)
-
-    Session.query(Request).filter(and_(
-        Request.status == "posted",
-        Request.user_id.in_(
-            Session.query(User.id)
-            .filter(User.last_online < datetime.now() - timedelta(7))
-        ),
-    )).update({"status": "draft"}, synchronize_session=False),
-    transaction.commit()
-    Session.close()
+    with db_session() as db:
+        db.query(Request).filter(and_(
+            Request.status == "posted",
+            Request.user_id.in_(
+                db.query(User.id)
+                .filter(User.last_online < func.now() - cast("7 days", INTERVAL))
+            ),
+        )).update({"status": "draft"}, synchronize_session=False)
 
 
 @app.task
 def update_request_tag_ids(request_id):
-    engine = engine_from_config(app.conf["PYRAMID_REGISTRY"].settings, "sqlalchemy.")
-    # TODO don't use scoped_session
-    Session.configure(bind=engine)
-    Session.query(Request).filter(Request.id == request_id).update({
-        "tag_ids": func.array(
-            Session.query(RequestTag.tag_id)
-            .filter(RequestTag.request_id == request_id)
-            .order_by(RequestTag.tag_id).subquery()
-        ),
-    }, synchronize_session=False)
-    transaction.commit()
-    Session.close()
+    with db_session() as db:
+        db.query(Request).filter(Request.id == request_id).update({
+            "tag_ids": func.array(
+                db.query(RequestTag.tag_id)
+                .filter(RequestTag.request_id == request_id)
+                .order_by(RequestTag.tag_id).subquery()
+            ),
+        }, synchronize_session=False)
+
