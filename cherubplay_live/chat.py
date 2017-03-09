188d0ff2276eaa465af45d199b50807087f590b1
Olly Parton <mysticdragonfly@hotmail.co.uk>
2017-03-09 23:28:56 +0000

    Update chat handler for Python 3.

--- cherubplay_live/chat.py
+++ cherubplay_live/chat.py
@@ -18,7 +18,7 @@ from tornadoredis import Client
 from cherubplay.lib import colour_validator, symbols
 from cherubplay.models import Chat, ChatUser, Message
 
-from db import config, get_chat, get_chat_user, get_user, publish_client, sm
+from cherubplay_live.db import config, get_chat, get_chat_user, get_user, publish_client, sm
 
 
 sockets = set()
@@ -31,7 +31,7 @@ class ChatHandler(WebSocketHandler):
 
     def open(self, chat_url):
         sockets.add(self)
-        print chat_url
+        print(chat_url)
         self.user = get_user(self.cookies)
         if self.user is None:
             self.close()
@@ -48,7 +48,7 @@ class ChatHandler(WebSocketHandler):
         # Fire online message, but only if this is the only tab we have open.
         online_symbols = set(int(_) for _ in publish_client.hvals("online:"+str(self.chat.id)))
         if self.chat_user.symbol not in online_symbols:
-            publish_client.publish("chat:"+str(self.chat.id), json.dumps({
+            publish_client.publish("chat:" + str(self.chat.id), json.dumps({
                 "action": "online",
                 "symbol": self.chat_user.symbol_character,
             }))
@@ -60,12 +60,12 @@ class ChatHandler(WebSocketHandler):
                 "action": "online",
                 "symbol": symbols[symbol],
             })
-        publish_client.hset("online:"+str(self.chat.id), self.socket_id, self.chat_user.symbol)
+        publish_client.hset("online:" + str(self.chat.id), self.socket_id, self.chat_user.symbol)
         self.redis_listen()
         self.ignore_next_message = False
         # Send the backlog if necessary.
         if "after" in self.request.query_arguments:
-            print "after"
+            print("after")
             try:
                 after = int(self.request.query_arguments["after"][0])
             except ValueError:
@@ -75,15 +75,15 @@ class ChatHandler(WebSocketHandler):
                 Message.chat_id == self.chat.id,
                 Message.id > after,
             )):
-                print message
+                print(message)
                 self.write_message({
                     "action": "message",
                     "message": {
-                        "id": message.id,
-                        "type": message.type,
+                        "id":     message.id,
+                        "type":   message.type,
                         "colour": message.colour,
                         "symbol": message.symbol_character,
-                        "text": message.text,
+                        "text":   message.text,
                     }
                 })
             Session.commit()
@@ -91,21 +91,21 @@ class ChatHandler(WebSocketHandler):
     def on_message(self, message_string):
         message = json.loads(message_string)
         if message["action"] in ("typing", "stopped_typing"):
-            publish_client.publish("chat:"+str(self.chat.id), json.dumps({
+            publish_client.publish("chat:" + str(self.chat.id), json.dumps({
                 "action": message["action"],
                 "symbol": self.chat_user.symbol_character,
             }))
             # Ignore our own typing messages.
             self.ignore_next_message = True
-        print message
+        print(message)
 
     def on_close(self):
         # Unsubscribe here and let the exit callback handle disconnecting.
-        self.redis_client.unsubscribe(("chat:"+str(self.chat.id), "user:"+str(self.user.id)))
-        publish_client.hdel("online:"+str(self.chat.id), self.socket_id)
+        self.redis_client.unsubscribe(("chat:" + str(self.chat.id), "user:" + str(self.user.id)))
+        publish_client.hdel("online:" + str(self.chat.id), self.socket_id)
         # Fire offline message, but only if we don't have any other tabs open.
-        if str(self.chat_user.symbol) not in publish_client.hvals("online:"+str(self.chat.id)):
-            publish_client.publish("chat:"+str(self.chat.id), json.dumps({
+        if str(self.chat_user.symbol) not in publish_client.hvals("online:" + str(self.chat.id)):
+            publish_client.publish("chat:" + str(self.chat.id), json.dumps({
                 "action": "offline",
                 "symbol": self.chat_user.symbol_character,
             }))
@@ -121,7 +121,7 @@ class ChatHandler(WebSocketHandler):
         if message.kind=="message":
             if not self.ignore_next_message:
                 self.write_message(message.body)
-                print "redis message:", message.body
+                print("redis message:", message.body)
             else:
                 self.ignore_next_message = False
 
@@ -130,12 +130,12 @@ class ChatHandler(WebSocketHandler):
 
 
 def sig_handler(sig, frame):
-    print "Caught signal %s." % sig
+    print("Caught signal %s." % sig)
     ioloop.add_callback_from_signal(shutdown)
 
 
 def shutdown():
-    print "Shutting down."
+    print("Shutting down.")
     for socket in sockets:
         ioloop.add_callback(socket.close)
     deadline = time.time() + 10
@@ -156,7 +156,7 @@ def main():
     application = Application([(r"/(.*)/", ChatHandler)])
 
     server = HTTPServer(application)
-    socket = bind_unix_socket(config.get("app:main", "cherubplay.socket_chat"), mode=0777)
+    socket = bind_unix_socket(config.get("app:main", "cherubplay.socket_chat"), mode=0o777)
     server.add_socket(socket)
 
     signal.signal(signal.SIGTERM, sig_handler)
