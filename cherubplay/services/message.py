1f97d12bb168a675252124d131974278904d5380
Olly Parton <mysticdragonfly@hotmail.co.uk>
2018-01-09 23:53:15 +0000

    Put publish error handling in its own method.

--- cherubplay/services/message.py
+++ cherubplay/services/message.py
@@ -1,12 +1,16 @@
-import datetime, json
+import datetime, json, logging
 
 from sqlalchemy import and_
+from typing import Dict, Union
 from zope.interface import Interface, implementer
 
 from cherubplay.lib import OnlineUserStore
-from cherubplay.models import Chat, ChatUser, ChatUserStatus, Message
+from cherubplay.models import ChatUser, ChatUserStatus, Message
 from cherubplay.models.enums import MessageType
 from cherubplay.tasks import trigger_push_notification
+
+
+log = logging.getLogger(__name__)
 
 
 class IMessageService(Interface):
@@ -31,6 +35,14 @@ class MessageService(object):
     def __init__(self, request):
         self._db     = request.find_service(name="db")
         self._pubsub = request.pubsub
+
+    def _publish(self, key: str, message: Union[str, Dict]):
+        if isinstance(message, dict):
+            message = json.dumps(message)
+        try:
+            self._pubsub.publish(key, message)
+        except ConnectionError:
+            log.error("Failed to send pubsub message.")
 
     def send_message(self, chat_user: ChatUser, type: MessageType, colour: str, text: str, action: str="message"):
         chat = chat_user.chat
@@ -72,48 +84,39 @@ class MessageService(object):
         chat_user.last_colour = colour
         chat_user.draft = ""
 
-        try:
-            # See if anyone else is online and update their ChatUser too.
-            online_handles = OnlineUserStore(self._pubsub).online_handles(chat)
-            for other_chat_user in self._db.query(ChatUser).filter(and_(
-                ChatUser.chat_id == chat.id,
-                ChatUser.status == ChatUserStatus.active,
-            )):
-                if other_chat_user.handle in online_handles:
-                    other_chat_user.visited = posted_date
-                # Only trigger notifications if the user has seen the most recent message.
-                # This stops us from getting multiple notifications about the same chat.
-                elif not most_recent_message or other_chat_user.visited > most_recent_message.posted:
-                    self._pubsub.publish("user:" + str(other_chat_user.user_id), json.dumps({
-                        "action": "notification",
-                        "url": chat.url,
-                        "title": other_chat_user.title or chat.url,
-                        "colour": colour,
-                        "symbol": chat_user.symbol_character,
-                        "name": chat_user.name,
-                        "text": notification_text if len(notification_text) < 100 else notification_text[:97] + "...",
-                    }))
-                    trigger_push_notification.delay(other_chat_user.user_id)
-        except ConnectionError:
-            pass
-
-        self._publish_message(new_message, chat_user, action)
-
-    def _publish_message(self, message: Message, chat_user: ChatUser, action="message"):
-        try:
-            self._pubsub.publish("chat:%s" % message.chat_id, json.dumps({
-                "action": action,
-                "message": {
-                    "id": message.id,
-                    "type": message.type.value,
-                    "colour": message.colour,
+        # See if anyone else is online and update their ChatUser too.
+        online_handles = OnlineUserStore(self._pubsub).online_handles(chat)
+        for other_chat_user in self._db.query(ChatUser).filter(and_(
+            ChatUser.chat_id == chat.id,
+            ChatUser.status == ChatUserStatus.active,
+        )):
+            if other_chat_user.handle in online_handles:
+                other_chat_user.visited = posted_date
+            # Only trigger notifications if the user has seen the most recent message.
+            # This stops us from getting multiple notifications about the same chat.
+            elif not most_recent_message or other_chat_user.visited > most_recent_message.posted:
+                self._publish("user:" + str(other_chat_user.user_id), {
+                    "action": "notification",
+                    "url": chat.url,
+                    "title": other_chat_user.title or chat.url,
+                    "colour": colour,
                     "symbol": chat_user.symbol_character,
                     "name": chat_user.name,
-                    "text": message.text,
-                },
-            }))
-        except ConnectionError:
-            pass
+                    "text": notification_text if len(notification_text) < 100 else notification_text[:97] + "...",
+                })
+                trigger_push_notification.delay(other_chat_user.user_id)
+
+        self._publish("chat:%s" % new_message.chat_id, {
+            "action": action,
+            "message": {
+                "id": new_message.id,
+                "type": new_message.type.value,
+                "colour": new_message.colour,
+                "symbol": chat_user.symbol_character,
+                "name": chat_user.name,
+                "text": new_message.text,
+            },
+        })
 
     def send_end_message(self, chat_user: ChatUser, deleted: bool=False):
         text = "%%s %s the chat." % ("deleted" if deleted else "ended")
@@ -127,7 +130,7 @@ class MessageService(object):
     def send_kick_message(self, kicking_chat_user: ChatUser, kicked_chat_user: ChatUser):
         text = "%s has been removed from the chat." % kicked_chat_user.name
         self.send_message(kicked_chat_user, MessageType.system, "000000", text, "message")
-        self._pubsub.publish(
+        self._publish(
             "chat:%s:user:%s" % (kicked_chat_user.chat_id, kicked_chat_user.user_id),
             "kicked",
         )
@@ -136,14 +139,12 @@ class MessageService(object):
     def send_change_name_message(self, chat_user: ChatUser, old_name: str):
         text = "%s is now %s." % (old_name, chat_user.name)
         self.send_message(chat_user, MessageType.system, "000000", text, "end")
-        try:
-            self._pubsub.publish("chat:" + str(chat_user.chat.id), json.dumps({
-                "action":   "name_change",
-                "old_name": old_name,
-                "new_name": chat_user.name,
-            }))
-        except ConnectionError:
-            pass
+        self._publish("chat:" + str(chat_user.chat.id), {
+            "action":   "name_change",
+            "old_name": old_name,
+            "new_name": chat_user.name,
+        })
+
 
 def includeme(config):
     config.register_service_factory(lambda context, request: MessageService(request), iface=IMessageService)
