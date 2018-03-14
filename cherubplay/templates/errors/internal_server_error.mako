54b54784d1323e213513301698c534d35e65d33c
Olly Parton <mysticdragonfly@hotmail.co.uk>
2018-03-14 23:41:36 +0000

    Custom 500 page.

---
+++ cherubplay/templates/errors/internal_server_error.mako
@@ -0,0 +1,3 @@
+<%inherit file="../base.mako" />\
+  <h2>500 Internal server error</h2>
+  <p>Sorry, something went wrong :(</p>
--- cherubplay/views/errors.py
+++ cherubplay/views/errors.py
@@ -1,12 +1,12 @@
 from pyramid.renderers import render_to_response
-from pyramid.view import forbidden_view_config, notfound_view_config
+from pyramid.view import view_config, forbidden_view_config, notfound_view_config
 
 
 @forbidden_view_config()
 def forbidden(request):
     if request.user is not None:
         resp = render_to_response("errors/forbidden.mako", {}, request=request)
-        resp.status = "403 Forbidden"
+        resp.status_int = 403
         return resp
     else:
         return render_to_response("layout2/home_guest.mako", {"forbidden": True}, request=request)
@@ -15,5 +15,12 @@ def forbidden(request):
 @notfound_view_config(append_slash=True)
 def not_found(request):
     resp = render_to_response("errors/not_found.mako", {}, request=request)
-    resp.status = "404 Not Found"
+    resp.status_int = 404
+    return resp
+
+
+@view_config(context=Exception)
+def internal_server_error(request):
+    resp = render_to_response("errors/internal_server_error.mako", {}, request=request)
+    resp.status_int = 500
     return resp
