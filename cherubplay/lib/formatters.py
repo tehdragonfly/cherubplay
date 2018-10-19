66372ea27b3600ae19cb2d586ce41ffacf543b04
Olly Parton <mysticdragonfly@hotmail.co.uk>
2018-10-19 21:56:47 +0100

    Start implementing a formatter descriptor.

--- cherubplay/lib/formatters.py
+++ cherubplay/lib/formatters.py
@@ -36,3 +36,29 @@ class RawFormatter(Formatter):
 message_formatters = {
     MessageFormat.raw: RawFormatter,
 }
+
+
+class FormattedValue:
+    def __init__(self, obj, format_attr, text_attr):
+        self._obj = obj
+        self._format_attr = format_attr
+        self._text_attr = text_attr
+
+    def as_plain_text(self) -> str:
+        # For notifications etc.
+        return getattr(self._obj, self._text_attr)
+
+    def as_html(self) -> Markup:
+        # For chat pages.
+        raise NotImplementedError
+
+
+class FormattedField:
+    def __init__(self, format_attr, text_attr):
+        self._format_attr = format_attr
+        self._text_attr = text_attr
+
+    def __get__(self, instance, owner):
+        if not instance:
+            raise ValueError("can only be accessed from an instance")
+        return FormattedValue(instance, self._format_attr, self._text_attr)
